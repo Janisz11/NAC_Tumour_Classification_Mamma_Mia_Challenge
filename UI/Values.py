9dcce8994836d4912142c98d173634206dabbeab
Krzysiek <krzysztof.nosek91@gmail.com>
2025-06-11 22:57:49 +0200

    Delete Values.py

--- UI/Values.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-root_folder =                       Path(__file__).resolve().parent
-images_folder_path =                root_folder / 'mamma_mia' / 'images'
-models_folder_path =                root_folder / 'mamma_mia' / 'models'
-clinical_and_imaging_info_path =    root_folder / 'mamma_mia' / 'clinical_and_imaging_info.xlsx'
